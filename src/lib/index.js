f044fe0256484c287e55458172e74fa35736684b
ptrushin <ptrushin@custis.ru>
2021-05-18 11:06:21 +0300

    -

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,10 @@
-import { loadCubeData, loadCubeRows } from "./CubeLoaderOdata";
+import { loadCubeData, loadCubeRows, getOdataFilter } from "./CubeLoaderOdata";
 import CubeViewer, {defaults} from "./CubeViewer";
 import DimensionViewAntdTable from './DimensionViewAntdTable';
 
 export { loadCubeData }
 export { loadCubeRows }
+export { getOdataFilter }
 export { CubeViewer }
 export { defaults }
 export { DimensionViewAntdTable }
